b61f141f0555bd5f2df136425bd0714085e76b6b
svigstol <sarah.vigstol@gmail.com>
2021-11-30 17:25:01 -0600

    begin chapter 10 exercise 4 assignment

---
+++ Ch10Ex4.py
@@ -0,0 +1,79 @@
+# Ch10Ex4.py
+# Ch10Ex4
+# Sarah Vigstol
+# This program will create three Employee objects and then display the data in each.
+
+# Import class
+import emp.py
+
+# Define main function
+def main():
+    # Call welcome function
+    welcome()
+
+    # Create three instances of Employee
+    emp1 =
+
+    emp2 =
+
+    emp3 =
+
+    print('Employee 1:')
+    print(	)
+    print()
+    print('Employee 2:')
+    print(	)
+    print()
+    print('Employee 3:')
+    print(	)
+
+# Define welcome message function
+welcome():
+    # Student name must be first line of output
+    print('Sarah Vigstol\n')
+
+# Call main function
+main()
+
+"""
+Output will go here
+
+------------------------ Class emp.py --------------------
+# Employee class
+
+class Employee:
+    def __init__(self, name, id_number, department, title):
+
+
+    def set_name(self, name):
+
+
+    def set_id_number(self, id_number):
+
+
+    def set_department(self, department):
+
+
+    def set_title(self, title):
+
+
+    def get_name(self):
+
+
+    def get_id_number(self):
+
+
+    def get_department(self):
+
+
+    def get_title(self):
+
+
+    def __str__(self):
+        result =
+
+
+
+        return result
+
+"""
